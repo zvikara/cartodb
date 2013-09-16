8e30094017f5744cc1a72cb3350ffe61379a4551
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-09-16 13:49:08 +0200

    CDB-514 #comment Implement WMS proxy service

---
+++ services/wms/proxy.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+require 'nokogiri'
+require 'typhoeus'
+
+module CartoDB
+  module WMS
+    class Proxy
+      SERVER_XPATH  = "//OnlineResource[1]"
+      FORMATS_XPATH = "//GetMap/Format"
+      LAYERS_XPATH  = "//Capability/Layer/Layer[1]/Layer/Title"
+
+      def initialize(url, preloaded_xml=nil)
+        @url        = url
+        @response   = preloaded_xml
+      end
+
+      def serialize
+        run
+        { server: server, formats: formats, layers: layers }
+      end
+
+      def run
+        request_capabilities unless response
+        self
+      end 
+
+      def request_capabilities
+        @response = Typhoeus.get(url).response_body
+        nil
+      end 
+
+      def document
+        Nokogiri::XML::Document.parse(response).remove_namespaces!
+      end
+
+      def server
+        document.at_xpath(SERVER_XPATH)['href']
+      end
+
+      def formats
+        document.xpath(FORMATS_XPATH).map(&:text)
+      end
+
+      def layers
+        document.xpath(LAYERS_XPATH).map { |element| 
+          { name: element.text, attribution: nil } 
+        }
+      end
+
+      attr_reader :response
+
+      private
+      
+      attr_reader :url
+    end # Proxy
+  end # WMS
+end # CartoDB
+
